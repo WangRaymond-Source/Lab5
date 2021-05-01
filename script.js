62cd62c51748178afb1fb090cafe1cd0f44a42f4
WangRaymond-Source <76501406+WangRaymond-Source@users.noreply.github.com>
2021-05-01 01:27:16 -0700

    Lab5

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ img.addEventListener('load', () => {
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
 //add Image to canvas
-addImgae.addEventListener('change', () =>{
+addImgae.addEventListener('event change', () =>{
   img.src = URL.createObjectURL(addImgae.file[0]);
-
+  img.alt = addImgae.file[0].name;
 });
 
 /**
